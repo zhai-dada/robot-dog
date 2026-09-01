62a61bc65ed25ae64e440fb9fbccba50993b6e98
zhai-dada <zhaipengwei0728@gmail.com>
2026-09-01 13:27:06 +0800

    feat: Complete Exercise 1-2 of Day 3

--- src/robot_dog_day3_services/src/state_client.cpp
+++ src/robot_dog_day3_services/src/state_client.cpp
@@ -5,6 +5,7 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "robot_dog_interfaces/srv/set_robot_state.hpp"
+#include "robot_dog_interfaces/srv/get_robot_state.hpp"
 
 class StateClient final : public rclcpp::Node
 {
@@ -13,63 +14,114 @@ public:
 	{
 		target_state_ = this->declare_parameter<std::string>("target_state", "TROT");
 		timeout_ms_ = this->declare_parameter<int>("timeout_ms", 3000);
+		status_flag_ = this->declare_parameter<bool>("status_flag", 1);
 
 		if (timeout_ms_ <= 0)
 		{
 			throw std::invalid_argument("timeout_ms must be greater than zero");
 		}
 
-		client_ = this->create_client<robot_dog_interfaces::srv::SetRobotState>(
-			"robot_dog/set_state");
+		if (status_flag_ == 1)
+		{
+			client_set_ = this->create_client<robot_dog_interfaces::srv::SetRobotState>(
+				"robot_dog/set_state");
+		}
+		else
+		{
+			client_get_ = this->create_client<robot_dog_interfaces::srv::GetRobotState>(
+				"robot_dog/get_state");
+		}
 	}
 
 	int sendRequest()
 	{
-		if (!client_->wait_for_service(std::chrono::milliseconds(timeout_ms_)))
+		if (status_flag_ == 1)
 		{
-			RCLCPP_ERROR(this->get_logger(), "服务不可用: /robot_dog/set_state");
-			return 1;
+			auto client_ = client_set_;
+			if (!client_->wait_for_service(std::chrono::milliseconds(timeout_ms_)))
+			{
+				RCLCPP_ERROR(this->get_logger(), "服务不可用: /robot_dog/set_state");
+				return 1;
+			}
+
+			auto request = std::make_shared<robot_dog_interfaces::srv::SetRobotState::Request>();
+			request->target_state = target_state_;
+
+			RCLCPP_INFO(this->get_logger(), "请求状态切换: target_state=%s", target_state_.c_str());
+
+			auto future = client_->async_send_request(request);
+
+			const auto result = rclcpp::spin_until_future_complete(
+				this->get_node_base_interface(), future,
+				std::chrono::milliseconds(timeout_ms_));
+
+			if (result != rclcpp::FutureReturnCode::SUCCESS)
+			{
+				RCLCPP_ERROR(this->get_logger(), "等待服务响应超时或失败");
+				return 1;
+			}
+
+			const auto response = future.get();
+			if (response->success)
+			{
+				RCLCPP_INFO(
+					this->get_logger(),
+					"服务成功: %s, current_state=%s",
+					response->message.c_str(), response->current_state.c_str());
+				return 0;
+			}
+
+			RCLCPP_WARN(
+				this->get_logger(),
+				"服务拒绝: %s, current_state=%s",
+				response->message.c_str(), response->current_state.c_str());
+			return 2;
 		}
-
-		auto request = std::make_shared<robot_dog_interfaces::srv::SetRobotState::Request>();
-		request->target_state = target_state_;
-
-		RCLCPP_INFO(this->get_logger(), "请求状态切换: target_state=%s", target_state_.c_str());
-		auto future = client_->async_send_request(request);
-		const auto result = rclcpp::spin_until_future_complete(
-			this->get_node_base_interface(), future,
-			std::chrono::milliseconds(timeout_ms_));
-
-		if (result != rclcpp::FutureReturnCode::SUCCESS)
+		else
 		{
-			RCLCPP_ERROR(this->get_logger(), "等待服务响应超时或失败");
-			return 1;
-		}
+			auto client_ = client_get_;
+			if (!client_->wait_for_service(std::chrono::milliseconds(timeout_ms_)))
+			{
+				RCLCPP_ERROR(this->get_logger(), "服务不可用: /robot_dog/get_state");
+				return 1;
+			}
 
-		const auto response = future.get();
-		if (response->success)
-		{
+			auto request = std::make_shared<robot_dog_interfaces::srv::GetRobotState::Request>();
+
+			auto future = client_->async_send_request(request);
+
+			const auto result = rclcpp::spin_until_future_complete(
+				this->get_node_base_interface(), future,
+				std::chrono::milliseconds(timeout_ms_));
+
+			if (result != rclcpp::FutureReturnCode::SUCCESS)
+			{
+				RCLCPP_ERROR(this->get_logger(), "等待服务响应超时或失败");
+				return 1;
+			}
+
+			const auto response = future.get();
+
 			RCLCPP_INFO(
 				this->get_logger(),
-				"服务成功: %s, current_state=%s",
-				response->message.c_str(), response->current_state.c_str());
+				"服务成功: current_state=%s battery=%.2f",
+				response->current_state.c_str(), response->battery_percentage);
+
 			return 0;
 		}
-
-		RCLCPP_WARN(
-			this->get_logger(),
-			"服务拒绝: %s, current_state=%s",
-			response->message.c_str(), response->current_state.c_str());
-		return 2;
+		return 0;
 	}
 
 private:
-	rclcpp::Client<robot_dog_interfaces::srv::SetRobotState>::SharedPtr client_;
+	rclcpp::Client<robot_dog_interfaces::srv::SetRobotState>::SharedPtr client_set_;
+	rclcpp::Client<robot_dog_interfaces::srv::GetRobotState>::SharedPtr client_get_;
+
 	std::string target_state_;
 	int timeout_ms_;
+	bool status_flag_;
 };
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
 	rclcpp::init(argc, argv);
 	int return_code = 0;
@@ -79,7 +131,7 @@ int main(int argc, char * argv[])
 		auto node = std::make_shared<StateClient>();
 		return_code = node->sendRequest();
 	}
-	catch (const std::exception & e)
+	catch (const std::exception &e)
 	{
 		RCLCPP_ERROR(rclcpp::get_logger("main"), "节点异常: %s", e.what());
 		return_code = 1;
--- src/robot_dog_day3_services/src/state_server.cpp
+++ src/robot_dog_day3_services/src/state_server.cpp
@@ -6,6 +6,7 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "robot_dog_interfaces/srv/set_robot_state.hpp"
+#include "robot_dog_interfaces/srv/get_robot_state.hpp"
 
 class StateServer final : public rclcpp::Node
 {
@@ -18,10 +19,18 @@ public:
 			throw std::invalid_argument("initial_state is not a supported robot state");
 		}
 
-		service_ = this->create_service<robot_dog_interfaces::srv::SetRobotState>(
+		status_set_service_ = this->create_service<robot_dog_interfaces::srv::SetRobotState>(
 			"robot_dog/set_state",
 			std::bind(
-				&StateServer::handleRequest,
+				&StateServer::handleRequestSet,
+				this,
+				std::placeholders::_1,
+				std::placeholders::_2));
+
+		status_get_status_set_service_ = this->create_service<robot_dog_interfaces::srv::GetRobotState>(
+			"robot_dog/get_state",
+			std::bind(
+				&StateServer::handleRequestGet,
 				this,
 				std::placeholders::_1,
 				std::placeholders::_2));
@@ -30,15 +39,20 @@ public:
 			this->get_logger(),
 			"状态服务端已启动: initial_state=%s service=/robot_dog/set_state",
 			current_state_.c_str());
+		RCLCPP_INFO(
+			this->get_logger(),
+			"状态服务端已启动: initial_state=%s service=/robot_dog/get_state",
+			current_state_.c_str());
 	}
 
 private:
 	bool isValidState(const std::string &state) const
 	{
-		static constexpr std::array<const char *, 4> valid_states = {
-			"STANDING", "SIT", "CRAWL", "TROT"};
+		static constexpr std::array<std::string_view, 4> valid_states = {
+			"STANDING", "SIT", "CRAWL", "TROT"
+		};
 
-		for (const char *valid_state : valid_states)
+		for (const std::string_view valid_state : valid_states)
 		{
 			if (state == valid_state)
 			{
@@ -49,7 +63,7 @@ private:
 		return false;
 	}
 
-	void handleRequest(
+	void handleRequestSet(
 		const robot_dog_interfaces::srv::SetRobotState::Request::SharedPtr request,
 		robot_dog_interfaces::srv::SetRobotState::Response::SharedPtr response)
 	{
@@ -61,17 +75,53 @@ private:
 			RCLCPP_WARN(this->get_logger(), "%s", response->message.c_str());
 			return;
 		}
-
-		const std::string previous_state = current_state_;
-		current_state_ = request->target_state;
-		response->success = true;
-		response->message = "state changed from " + previous_state + " to " + current_state_;
-		response->current_state = current_state_;
+		if(current_state_ == "STANDING" && request->target_state == "STANDING")
+		{
+			response->message = current_state_ + " can not convert to " + request->target_state;
+			response->success = false;
+			response->current_state = current_state_;
+		}
+		else if(current_state_ == "SIT" && request->target_state != "STANDING")
+		{
+			response->message = current_state_ + " can not convert to " + request->target_state;
+			response->success = false;
+			response->current_state = current_state_;
+		}
+		else if(current_state_ == "CRAWL" && (request->target_state != "STANDING" && request->target_state != "TROT"))
+		{
+			response->message = current_state_ + " can not convert to " + request->target_state;
+			response->success = false;
+			response->current_state = current_state_;
+		}
+		else if(current_state_ == "TROT" && (request->target_state != "STANDING" && request->target_state != "CRAWL"))
+		{
+			// throw std::invalid_argument(current_state_ + " can not convert to " + request->target_state);
+			response->message = current_state_ + " can not convert to " + request->target_state;
+			response->success = false;
+			response->current_state = current_state_;
+		}
+		else
+		{
+			response->message = "state changed from " + current_state_ + " to " + request->target_state;
+			current_state_ = request->target_state;
+			response->success = true;
+			response->current_state = current_state_;
+		}
 
 		RCLCPP_INFO(this->get_logger(), "%s", response->message.c_str());
 	}
 
-	rclcpp::Service<robot_dog_interfaces::srv::SetRobotState>::SharedPtr service_;
+	void handleRequestGet(
+		const robot_dog_interfaces::srv::GetRobotState::Request::SharedPtr request,
+		robot_dog_interfaces::srv::GetRobotState::Response::SharedPtr response)
+	{
+		response->current_state = current_state_;
+		response->battery_percentage = 66.66;
+	}
+
+	rclcpp::Service<robot_dog_interfaces::srv::SetRobotState>::SharedPtr status_set_service_;
+	rclcpp::Service<robot_dog_interfaces::srv::GetRobotState>::SharedPtr status_get_status_set_service_;
+
 	std::string current_state_;
 };
 
@@ -87,6 +137,7 @@ int main(int argc, char *argv[])
 	catch (const std::exception &e)
 	{
 		RCLCPP_ERROR(rclcpp::get_logger("main"), "节点异常: %s", e.what());
+		rclcpp::shutdown();
 		return_code = 1;
 	}
 
